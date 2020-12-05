64b008e8d23cc5deb460fb95ed40db14b1c40ebf
Blake Hillier <blakehillier@mac.com>
2020-12-04 21:38:38 -0800

    Changed pays to countries

--- Risk.py
+++ Risk.py
@@ -9,12 +9,12 @@ class Continent():
 		self.name=name
 		self.country=[]
 		for p in country:
-			P=Pays(p,name,Map)
+			P=Country(p,name,Map)
 			self.country.append(P)
 			Map.country.append(P)
 		self.nb_country=len(self.country)
 
-class Pays():
+class Country():
 	def __init__(self,name,continent,Map):
 		Map.nb_country=Map.nb_country+1
 		self.id=Map.nb_country
@@ -46,6 +46,7 @@ class Player():
 		self.obj=None
 		self.cards=[]
 		self.win_land=False
+        self.is_ai=False
 
 	def use_best_cards(self):
 		# TODO Prevent use if not in deployment phase
@@ -114,6 +115,53 @@ class Player():
 			#print(self.name+" is dead")
 			#self.turns.order.remove(self.id_player)
 			return False
+
+# This class will manage the AI for the game by hijaking the player class
+class AI(Player):
+    def __init__(self):
+        self.is_ai=True
+
+    def placement(self):
+      """
+      self.__find_wars(attack=True)
+      While reinforcments > 0:
+        Add troop to country with lowest chance 
+        Recalculate odds and re-sort
+      Return list of (country, reinforcment) pairs?
+      """
+
+    def attack(self):
+      """
+      create list of wars from self.__find_wars(attack=True)
+      while time:
+        remove wars with low success rates
+        Use UCB1 to select a path leading to new node
+        ** We can copy the origional list, 
+           remove the war which just happened, 
+           Add wars with new neighbors **
+        simulate rest of wars until no good success rates
+        backtrack
+      return path
+      ** should terminate early if a battle is lost in actual game
+      """
+
+    def deplacement(self):
+      """
+      self.__find_wars(attack=False)
+      Find country with highest and lowest chances
+      Split reinforcments between the two
+      Return pair of (country, reinforcments) pairs
+      """
+
+    # Find list of possible wars and their chances of success for attack (if true)
+    # or defence (if false)
+    def __find_wars(self, attack=True):
+      """
+      For each country:
+        For each enemy neighbor:
+          save (neighbor, chance of success)
+      return list of pairs
+      """
 
 # Types of missions(?) players are able to perform
 class Goal():
@@ -275,20 +323,22 @@ class Card():
 		return str(self.type)
 
 class Turns():
-	def __init__(self,nb_players,M):
+	def __init__(self,nb_players,M,nb_ai):
 		self.game_finish=False
 		self.num=0
 		self.nb_players=nb_players
-		self.order=list(range(1,nb_players+1))
+		self.order=list(range(1,nb_players+nb_ai+1))
 		random.shuffle(self.order)
 		self.nb_country=M.nb_country
 		self.players=[]
 		# Generation of players
 		for k in range(0,nb_players):
 			self.players.append(Player(k+1,M,self))
+        for k in range(nb_players, nb_ai+nb_players):
+            self.players.append(AI(k+1,M,self))
 		# Assignment of objective after players are made
 		self.goal=Goal(M,self)
-		for k in range(0,nb_players):
+		for k in range(0,nb_players+nb_ai):
 			self.players[k].obj=Objective(self.goal,self.players[k])
 		self.id_order=0
 		self.map=M
