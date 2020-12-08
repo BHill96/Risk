2c0642ae7db5612fba96944d7fb58ca019953fd5
Blake Hillier <blakehillier@mac.com>
2020-12-07 18:05:47 -0800

    Now finds contiguous chunks of land for deplacement phase

--- AI.py
+++ AI.py
@@ -8,6 +8,7 @@ This file manages the AI's actions for the game.
 import random
 import copy
 from Player import *
+from Map import *
 
 class AI(Player):
     def __init__(self,id,Map,turns):
@@ -21,7 +22,6 @@ class AI(Player):
       wars = self.__find_wars(attack=True)
       wars = sorted(wars, key=lambda war:war[2])
       # While reinforcments > 0:
-      print("troops",self.nb_troops)
       troops = self.nb_troops
       # Dict of country name:troops to add
       reinforce = {}
@@ -54,12 +54,64 @@ class AI(Player):
       """
 
     def deplacement(self):
-      """
-      self.__find_wars(attack=False)
-      Find country with highest and lowest chances
-      Split reinforcments between the two
-      Return pair of (country, reinforcments) pairs
-      """
+      print("AI deplacement")
+      # Find wars and success rates
+      wars = self.__find_wars(attack=False)
+      wars = sorted(wars, key=lambda war:war[2])
+      # Find possible paths within contiguous territory 
+      paths = self.__find_deplacement_paths()
+      print("Paths:",paths)
+      return None
+
+      """# Find country with highest and lowest chances
+      high = wars[0]
+      low = wars[-1]
+      m = Map('Terre')
+      print("deplacement", high[0].name, low[0].name)
+      i = 1
+      while not m.chemin_exist(self.country, high[0], low[0]) and i < len(wars):
+        high = wars[i]
+        i += 1
+      if high[2] < threshold:
+        high = wars[0]
+        i = len(wars-2)
+        while not m.chemin_exist(self.country, high[0], low[0]) and i > 0:
+          low = wars[i]
+          i -= 1
+
+      print("Moving from {0} to {1}".format(high[0].name, low[0].name))
+      # Split reinforcments between the two
+      oldDiff = abs(high[2]-low[2])
+      move = [high[0].id, 1, low[0].id]
+      high, low = (high[0], high[1], high[2]-0.1), (low[0], low[1], low[2]+0.1)
+      newDiff = abs(high[2]-low[2])
+      print("  equalized odds from {0} to {1}".format(oldDiff, newDiff))
+      while newDiff < oldDiff:
+        oldDiff = newDiff
+        move[1] += 1
+        high, low = (high[0], high[1], high[2]-0.1), (low[0], low[1], low[2]+0.1)
+        newDiff = abs(high[2]-low[2])
+        print("  equalized odds from {0} to {1}".format(oldDiff, newDiff))
+      # Return pair of (country, reinforcments) pairs
+      return move"""
+
+    # Finds paths within contiguous country groups
+    def __find_deplacement_paths(self):
+      reachable = []
+      used = []
+      for c in self.country:
+        if c not in used:
+          used.append(c)
+          country = self.__find_country(c)
+          tmp = [c]
+          for n in self.country:
+            if n != c and self.map.chemin_exist(self.country, country, self.__find_country(n)):
+              tmp.append(n)
+              used.append(n)
+            else:
+          if len(tmp) > 1:
+            reachable.append(tmp)
+      return reachable
 
     # Find list of possible wars and their chances of success for attack (if true)
     # or defence (if false)
@@ -68,15 +120,18 @@ class AI(Player):
       # For each country:
       for c in self.country:
         country=self.__find_country(c)
-        print(country.name)
         # For each enemy neighbor:
         for n in country.neighbor:
           neighbor=self.__find_country(n)
           if country.id_player != neighbor.id_player:
             # Find chance of winning war
-            success_chance = random.uniform(0,1)
+            if attack:
+              # Success based on attack
+              success_chance = random.uniform(0,1)
+            else:
+              # Success based on defense
+              success_chance = random.uniform(0,1)
             # save (neighbor, chance of success)
-            print("  {2}::{0}::{1}".format(neighbor.name, success_chance, n))
             wars.append((country, neighbor, success_chance))
       # return list of pairs
       return wars
